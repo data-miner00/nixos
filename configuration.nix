64f0a622c9683a2da407caff1e9f8be6382bf323
Shaun Chong <pkay_@live.com>
2024-10-04 23:38:35 +0800

    fix wrong displayManager section

--- configuration.nix
+++ configuration.nix
@@ -37,25 +37,28 @@
   environment.pathsToLink = [ "/libexec" ];
 
   # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-
-    desktopManager = {
-      xterm.enable = false;
-    };
+  services = {
 
     displayManager = {
       defaultSession = "none+i3";
     };
-  
-    windowManager.i3 = {
+    
+    xserver = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-        i3blocks
-      ];
+
+      desktopManager = {
+        xterm.enable = false;
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu
+          i3status
+          i3lock
+          i3blocks
+        ];
+      };
     };
   };
 
