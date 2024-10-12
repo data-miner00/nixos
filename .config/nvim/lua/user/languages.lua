8e0499ebb2c470ef805400f777f47f1e72bf839f
Shaun Chong <pkay_@live.com>
2024-10-12 22:13:33 +0800

    Move parser to separate module

---
+++ .config/nvim/lua/user/languages.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.servers = {
+  "lua_ls",
+  "cssls",
+  "tsserver",
+  "jsonls",
+  "clangd"
+}
+
+M.parsers = {
+  "lua",
+  "markdown",
+  "javascript",
+  "typescript",
+  "html",
+  "css",
+  "json",
+  "yaml",
+}
+
+return M
