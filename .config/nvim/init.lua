ba55a029eb8b1922834754fe856f253f3d41e4ec
Shaun Chong <pkay_@live.com>
2024-10-12 16:46:05 +0800

    move to user module

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,8 @@
 -- NVIM v0.9.1
 
-require("settings")
-require("keymaps")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-local opts = {}
-require("lazy").setup("plugins", opts)
+require("user.settings")
+require("user.keymaps")
+require("user.lazy-setup")
 
 local function print_plugins()
   local plugins = require("lazy").plugins()
---
+++ .config/nvim/lua/user/settings.lua
@@ -0,0 +1,23 @@
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 2
+vim.opt.wrap = false
+vim.opt.path = vim.opt.path + "**"
